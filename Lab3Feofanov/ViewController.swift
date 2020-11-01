0377f6eb1a0a0c1b65dc1353fd07ed5935c9988e
Gemofort <vanya6677@gmail.com>
2020-11-01 23:54:03 +0200

    Initial Commit

---
+++ Lab3Feofanov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab3Feofanov
+//
+//  Created by Ivan Feofanov on 01.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
